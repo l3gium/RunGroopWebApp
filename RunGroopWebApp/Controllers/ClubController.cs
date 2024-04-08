662df83d95be3b1ba6cfd7941682b53f7637dd99
l3gium <miguel.luiztto@gmail.com>
2024-04-08 20:40:51 -0300

    controller e view de race e club

---
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        public ClubController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
