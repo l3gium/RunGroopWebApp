52180f53a1cd3ca9d02fc998bb78727b4d0745a9
l3gium <miguel.luiztto@gmail.com>
2024-04-11 13:46:51 -0300

    view e controller de criação de clube

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -27,5 +27,21 @@ namespace RunGroopWebApp.Controllers
             ClubModel club = await _clubRepository.GetByIdAsync(id);
             return View(club);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ClubModel club)
+        {
+            if(!ModelState.IsValid)
+                return View(club);
+
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
+
     }
 }
